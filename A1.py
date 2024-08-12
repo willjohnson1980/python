e2c51021d021d09eab2736beaa2732b6c7a0ae74
willjohnson1980 <william.s.johnson3285@gmail.com>
2024-08-12 09:22:32 -0500

    Update A1.py
    Added descriptive notes

--- A1.py
+++ A1.py
@@ -1,12 +1,16 @@
 class Restaurant:
+    #A class representing a restaurant
     def __init__(self, name, cuisine_type):
+        #Initialize the restaurant
         self.name = name
         self.cuisine_type = cuisine_type
         
     def describe_restaurant(self):
+        #Display a summary of the restaurant
         print('The name of the Restaurant is {} and it makes {}'.format(self.name, self.cuisine_type))
     
     def open_restaurant(self):
+        #Display a message that the restaurant is open
         print('The Restaurant is open')
         
 
@@ -19,19 +23,19 @@ restaurant.describe_restaurant()
 restaurant.open_restaurant()
 
 class Restaurant:
-
+#A class representing a restaurant
     def __init__(self, name, cuisine_type):
-        
+        #Initialize the restaurant
         self.name = name.title()
         self.cuisine_type = cuisine_type
 
     def describe_restaurant(self):
-
+#Display a summary of the restaurant
         msg = f"{self.name} serves wonderful {self.cuisine_type}."
         print(f"\n{msg}")
 
     def open_restaurant(self):
-
+#Display a message that the restaurant is open
         msg = f"{self.name} is open. Come on in!"
         print(f"\n{msg}")
 
@@ -45,9 +49,9 @@ mango_thai = Restaurant('mango thai', 'thai food')
 mango_thai.describe_restaurant()
 
 class User:
-
+#Represent a simple user profile
     def __init__(self, first_name, last_name, username, email, location):
-    
+    #Initialize the user
         self.first_name = first_name.title()
         self.last_name = last_name.title()
         self.username = username
@@ -55,14 +59,14 @@ class User:
         self.location = location.title()
 
     def describe_user(self):
-    
+    #Display a summary of the user's information
         print(f"\n{self.first_name} {self.last_name}")
         print(f"  Username: {self.username}")
         print(f"  Email: {self.email}")
         print(f"  Location: {self.location}")
 
     def greet_user(self):
-
+#Display a personalized greeting to the user
         print(f"\nWelcome back, {self.username}!")
 
 eric = User('eric', 'matthes', 'e_matthes', 'e_matthes@example.com', 'alaska')
