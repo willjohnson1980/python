2bf24f6091943e10f7312c0d5ab8f0bae7813df4
willjohnson1980 <william.s.johnson3285@gmail.com>
2024-08-12 09:49:36 -0500

    Update A3.py
    Added descriptive text

--- A3.py
+++ A3.py
@@ -1,12 +1,16 @@
 class Restaurant:
+    #A class representing a restaurant
     def __init__(self, name, cuisine_type):
+        #Initialize the restaurant
         self.name = name
         self.cuisine_type = cuisine_type
         
     def describe_restaurant(self):
+        #Display a summary of the restaurant
         print('The name of the Restaurant is {} and it makes {}'.format(self.name, self.cuisine_type))
     
     def open_restaurant(self):
+        #Display a message that the restaurant is open
         print('The Restaurant is open')
         
 
@@ -19,19 +23,19 @@ restaurant.describe_restaurant()
 restaurant.open_restaurant()
 
 class Restaurant:
-
+#A class representing a restaurant
     def __init__(self, name, cuisine_type):
-        
+        #Initialize the restaurant
         self.name = name.title()
         self.cuisine_type = cuisine_type
 
     def describe_restaurant(self):
-
+#Display a summary of the restaurant
         msg = f"{self.name} serves wonderful {self.cuisine_type}."
         print(f"\n{msg}")
 
     def open_restaurant(self):
-
+#Display a message that the restaurant is open
         msg = f"{self.name} is open. Come on in!"
         print(f"\n{msg}")
 
@@ -45,9 +49,9 @@ mango_thai = Restaurant('mango thai', 'thai food')
 mango_thai.describe_restaurant()
 
 class User:
-
+#Represent a simple user profile
     def __init__(self, first_name, last_name, username, email, location):
-    
+    #Initialize the user
         self.first_name = first_name.title()
         self.last_name = last_name.title()
         self.username = username
@@ -55,14 +59,14 @@ class User:
         self.location = location.title()
 
     def describe_user(self):
-    
+    #Display a summary of the user's information
         print(f"\n{self.first_name} {self.last_name}")
         print(f"  Username: {self.username}")
         print(f"  Email: {self.email}")
         print(f"  Location: {self.location}")
 
     def greet_user(self):
-
+#Display a personalized greeting to the user
         print(f"\nWelcome back, {self.username}!")
 
 eric = User('eric', 'matthes', 'e_matthes', 'e_matthes@example.com', 'alaska')
@@ -74,29 +78,29 @@ willie.describe_user()
 willie.greet_user()
 
 class Restaurant:
-
+#A class representing a restaurant
     def __init__(self, name, cuisine_type):
-        
+       #Initialize the restaurant 
         self.name = name.title()
         self.cuisine_type = cuisine_type
         self.number_served = 0
 
     def describe_restaurant(self):
-        
+        #Display a summary of the restaurant
         msg = f"{self.name} serves wonderful {self.cuisine_type}."
         print(f"\n{msg}")
 
     def open_restaurant(self):
-        
+        #Display a message that the restaurant is open
         msg = f"{self.name} is open. Come on in!"
         print(f"\n{msg}")
 
     def set_number_served(self, number_served):
-        
+        #Allow user to set the number of customers that have been served
         self.number_served = number_served
 
     def increment_number_served(self, additional_served):
-        
+        #Allow user to increment the number of customers served
         self.number_served += additional_served
 
 
@@ -114,10 +118,10 @@ restaurant.increment_number_served(250)
 print(f"Number served: {restaurant.number_served}")
 
 class User:
-    
+    #Represent a simple user profile
 
     def __init__(self, first_name, last_name, username, email, location):
-        
+        #Initialize the user
         self.first_name = first_name.title()
         self.last_name = last_name.title()
         self.username = username
@@ -126,22 +130,22 @@ class User:
         self.login_attempts = 0
 
     def describe_user(self):
-        
+      #Display a summary of the user's information
         print(f"\n{self.first_name} {self.last_name}")
         print(f"  Username: {self.username}")
         print(f"  Email: {self.email}")
         print(f"  Location: {self.location}")
 
     def greet_user(self):
-        
+        #Display a personalized greeting to the user
         print(f"\nWelcome back, {self.username}!")
 
     def increment_login_attempts(self):
-        
+        #Increment the value of login_attempts
         self.login_attempts += 1
 
     def reset_login_attempts(self):
-        
+       #Reset login_attempts to 0 
         self.login_attempts = 0
 
 eric = User('eric', 'matthes', 'e_matthes', 'e_matthes@example.com', 'alaska')
@@ -159,42 +163,42 @@ eric.reset_login_attempts()
 print(f"  Login attempts: {eric.login_attempts}")
 
 class Restaurant:
-
+#A class representing a restaurant
     def __init__(self, name, cuisine_type):
-        
+        #Initialize the restaurant
         self.name = name.title()
         self.cuisine_type = cuisine_type
         self.number_served = 0
 
     def describe_restaurant(self):
-        
+        #Display a summary of the restaurant
         msg = f"{self.name} serves wonderful {self.cuisine_type}."
         print(f"\n{msg}")
 
     def open_restaurant(self):
-        
+       #Display a message that the restaurant is open 
         msg = f"{self.name} is open. Come on in!"
         print(f"\n{msg}")
 
     def set_number_served(self, number_served):
-        
+        #Allow user to set the number of customers that have been served
         self.number_served = number_served
 
     def increment_number_served(self, additional_served):
-        
+        #Allow user to increment the number of customers served
         self.number_served += additional_served
 
 
 class IceCreamStand(Restaurant):
-    
+    #Represent an ice cream stand
 
     def __init__(self, name, cuisine_type='ice cream'):
-        
+        #Initialize an ice cream stand
         super().__init__(name, cuisine_type)
         self.flavors = []
 
     def show_flavors(self):
-        
+        #Display the flavors available
         print("\nWe have the following flavors available:")
         for flavor in self.flavors:
             print(f"- {flavor.title()}")
@@ -207,9 +211,9 @@ big_one.describe_restaurant()
 big_one.show_flavors()
 
 class User:
-
+#Represent a simple user profile
     def __init__(self, first_name, last_name, username, email, location):
-        
+      #Initialize the user 
         self.first_name = first_name.title()
         self.last_name = last_name.title()
         self.username = username
@@ -218,34 +222,34 @@ class User:
         self.login_attempts = 0
 
     def describe_user(self):
-        
+        #Display a summary of the user's information
         print(f"\n{self.first_name} {self.last_name}")
         print(f"  Username: {self.username}")
         print(f"  Email: {self.email}")
         print(f"  Location: {self.location}")
 
     def greet_user(self):
-        
+        #Display a personalized greeting to the user
         print(f"\nWelcome back, {self.username}!")
 
     def increment_login_attempts(self):
-        
+        #Increment the value of login_attempts
         self.login_attempts += 1
 
     def reset_login_attempts(self):
-        
+        #Reset login_attempts to 0
         self.login_attempts = 0
 
 
 class Admin(User):
-
+#A user with administrative privileges
     def __init__(self, first_name, last_name, username, email, location):
-        
+        #Initialize the admin
         super().__init__(first_name, last_name, username, email, location)
         self.privileges = []
 
     def show_privileges(self):
-        
+       #Display the privileges this administrator has 
         print("\nPrivileges:")
         for privilege in self.privileges:
             print(f"- {privilege}")
@@ -263,9 +267,9 @@ eric.privileges = [
 eric.show_privileges()
 
 class User:
-
+#Represent a simple user profile
     def __init__(self, first_name, last_name, username, email, location):
-        
+       #Initialize the user 
         self.first_name = first_name.title()
         self.last_name = last_name.title()
         self.username = username
@@ -274,30 +278,30 @@ class User:
         self.login_attempts = 0
 
     def describe_user(self):
-        
+       #Display a summary of the user's information
         print(f"\n{self.first_name} {self.last_name}")
         print(f"  Username: {self.username}")
         print(f"  Email: {self.email}")
         print(f"  Location: {self.location}")
 
     def greet_user(self):
-        
+        #Display a personalized greeting to the user
         print(f"\nWelcome back, {self.username}!")
 
     def increment_login_attempts(self):
-        
+        #Increment the value of login_attempts
         self.login_attempts += 1
 
     def reset_login_attempts(self):
-        
+        #Reset login_attempts to 0
         self.login_attempts = 0
 
 
 class Admin(User):
-    
+    #A user with administrative privileges
 
     def __init__(self, first_name, last_name, username, email, location):
-        
+        #Initialize the admin
         super().__init__(first_name, last_name, username, email, location)
 
         # Initialize an empty set of privileges.
@@ -305,7 +309,7 @@ class Admin(User):
 
 
 class Privileges:
-    
+    #A class to store an admin's privileges
 
     def __init__(self, privileges=[]):
         self.privileges = privileges
