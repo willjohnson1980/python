cc29d702efc016463c382981f21e1dc103a0a6ca
willjohnson1980 <william.s.johnson3285@gmail.com>
2024-08-12 09:56:10 -0500

    Update earthquake1.py
    Added descriptive text

--- earthquake1.py
+++ earthquake1.py
@@ -1,3 +1,4 @@
+#plot a simple graph of earthquake magnitutes
 import matplotlib.pyplot as plt
 magnitudes = [5,2, 6.3, 6.8, 4.8, 4.4, 5.1, 4.9, 5.7, 6.2, 7.4, 6.4, 7.1, 6.6] 
 # magnitudes from the past month from the GeoJSON Summary
