f79ec893245420a79722500c06ea3af7fb3cc09a
willjohnson1980 <william.s.johnson3285@gmail.com>
2024-08-12 09:54:11 -0500

    Update Cube.py
    Added descriptive text

--- Cube.py
+++ Cube.py
@@ -1,3 +1,6 @@
+#A number raised to the third power is a cube. Plot the first five cubic numbers, and then plot the first 5000 cubic numbers.
+
+Plotting 5 cubes:
 from matplotlib import pyplot as plt
  
 # Define data.
